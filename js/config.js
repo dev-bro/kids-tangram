9854ae2488f5d96f3e4ed564610a4c9e1a59e29b
John Doe <John@Doe.com>
2016-07-12 13:59:26 +0300

    Initial

---
+++ js/config.js
@@ -0,0 +1,96 @@
+var _fgq = [];
+
+famobi = new fg_api(
+	{
+		"features": { "highscores": 0, "menu": 1, "fullscreen": 1, "screenshot": 0, "payment": 0, "ads": 1 },
+		"game_i18n": {
+			"default": {
+				"filename.png": "fg_i18n\/{lang}\/images\/filename.png",
+				"api.back": "&laquo; Back",
+				"api.more": "&raquo; More Games",
+				"api.fullscreen": "Fullscreen mode",
+				"api.payment": "Buy Credits",
+				"api.close": "Close",
+				"api.ad_modal_header": "Advertisement closes in&hellip;",
+				"api.ad_modal_header2": "Advertisement&hellip;",
+				"api.teaser_modal_header": "Continue with the next episode&hellip;",
+				"api.payment_success": "Payment successful!",
+				"api.payment_list": "Purchase List",
+				"api.payment_items": "Items",
+				"api.payment_credits_available": "Credits available",
+				"api.screenshot": "Screenshot",
+				"api.continue": "Continue",
+				"api.play_now": "Play now",
+				"api.previous": "Previous",
+				"api.next": "Next Game",
+				"api.home": "Home",
+//						"more_games_image": "\/portal\/4638e320-4444-4514-81c4-d80a8c662371\/564f2b86a3bf7\/more-games-button\/600x253.png",
+//						"more_games_url": "http:\/\/www.games1.com",
+				"preload_image": "\/html5games\/gameapi\/v1\/invisPreloadImage.png",
+				"test_preload_image": "\/html5games\/gameapi\/v1\/testPreloadImage.png"
+			}
+		},
+		"gameParams": {
+			"languages_available": ["de", "tr", "en"],
+			"flags": { "exclusive": 1 },
+			"aspectRatio": 1.183,
+			"fullscreen_enabled": 1,
+			"header_image": "KidsTangramHeader.jpg"
+		},
+		"urlRoot": "http:\/\/play.famobi.com",
+		"assetsPath": "http:\/\/play.famobi.com\/assets\/0.1.32-26a6aed8",
+		"ts": 1464533440000,
+		"short_url": "http:\/\/play.famobi.com\/kids-tangram",
+		"uuid": "46556e46-e423-4880-bed1-ed0b68b8c76e",
+		"pid": "4638e320-4444-4514-81c4-d80a8c662371",
+		"aid": "A1000-1",
+		"name": "\"Kids Tangram\"",
+		"languages": [],
+		"ads": {
+			"adsense_channels": ["7657524996", ""],
+			"adx_channels": [""],
+			"min_s_between": 90,
+			"min_s_before": 15,
+			"show_initial": false,
+			"show_video": true,
+			"description_url": "http:\/\/api.famobi.com\/sda\/description\/kids-tangram?hl=en",
+			"provider": "dfp",
+			"dfp_available": false
+		},
+		"i18n": {
+			"default": {
+				"api.back": "&laquo; Back",
+				"api.more": "&raquo; More Games",
+				"api.fullscreen": "Fullscreen mode",
+				"api.payment": "Buy Credits",
+				"api.close": "Close",
+				"api.ad_modal_header": "Advertisement closes in&hellip;",
+				"api.ad_modal_header2": "Advertisement&hellip;",
+				"api.teaser_modal_header": "Continue with the next episode&hellip;",
+				"api.payment_success": "Payment successful!",
+				"api.payment_list": "Purchase List",
+				"api.payment_items": "Items",
+				"api.payment_credits_available": "Credits available",
+				"api.screenshot": "Screenshot",
+				"api.continue": "Continue",
+				"api.play_now": "Play now",
+				"api.previous": "Previous",
+				"api.next": "Next Game",
+				"api.home": "Home"
+			}
+		},
+		"branding": {
+			"more_games_image": "\/portal\/4638e320-4444-4514-81c4-d80a8c662371\/564f2b86a3bf7\/more-games-button\/600x253.png",
+			"more_games_url": "http:\/\/www.games1.com",
+			"preload_image": "\/html5games\/gameapi\/v1\/invisPreloadImage.png",
+			"test_preload_image": "\/html5games\/gameapi\/v1\/testPreloadImage.png"
+		},
+		"thumb": "https:\/\/img.cdn.famobi.com\/portal\/html5games\/images\/tmp\/KidsTangramTeaser.jpg",
+		"favicon_32": "https:\/\/img.cdn.famobi.com\/portal\/html5games\/images\/tmp\/32\/KidsTangramTeaser.jpg",
+		"favicon_64": "https:\/\/img.cdn.famobi.com\/portal\/html5games\/images\/tmp\/64\/KidsTangramTeaser.jpg",
+		"favicon_96": "https:\/\/img.cdn.famobi.com\/portal\/html5games\/images\/tmp\/96\/KidsTangramTeaser.jpg",
+		"favicon_192": "https:\/\/img.cdn.famobi.com\/portal\/html5games\/images\/tmp\/192\/KidsTangramTeaser.jpg",
+		"style": "\t<style type=\"text\/css\">\n\t\t\n\t<\/style>",
+//				"headerHtml": "<header id=\"fg-header\">\n\t<div id=\"fg-back\" class=\"icon-arrow icon-arrow-left\" data-fg-module=\"navigation\" data-fg-method=\"show\"></div>\n\t<div id=\"fg-logo\"><img src=\"https://games.cdn.famobi.com/html5games/branding/default/logo.png\" alt=\"\"></div>\n\t<div class=\"fg-clip\" id=\"fg-clip\">\n\t\t<div class=\"fg-clip-btn\"><img src=\"https://games.cdn.famobi.com/html5games/branding/default/icon.png\" alt=\"\"></div>\n\t</div>\n</header>",
+//				"menuHtml": "<ul><li data-famobi-href=\"back\"><a href=\"javascript:void(0);\" data-i18n=\"api.back\"><\/a><\/li><li data-famobi-href=\"moreGames\"><a href=\"javascript:void(0);\" data-i18n=\"api.more\"><\/a><\/li><li data-famobi-fullscreen><a href=\"javascript:void(0);\" data-i18n=\"api.fullscreen\"><\/a><\/li><\/ul>"
+	}, _fgq, '');
